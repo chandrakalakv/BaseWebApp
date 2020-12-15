5f0b2ccbdd5d77bcca9a739d8ea96c17db207f7c
chandrakala <chandrakala1391@gmail.com>
2020-12-15 15:31:18 -0500

    First commit

--- project/js/main.js
+++ project/js/main.js
@@ -1,3 +1,69 @@
+$(document).ready(function(){
+  getweather();
+})
+
+function handleSignIn(){
+  var provider = new firebase.auth.GoogleAuthProvider();
+  firebase.auth().signInWithPopup(provider).then(function(result) {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    var token = result.credential.accessToken;
+    // The signed-in user info.
+    var user = result.user;
+    console.log(user.email);
+  }).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // The email of the user's account used.
+    var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    var credential = error.credential;
+    // ...
+  });
+  
+}
+
+function addMessage(postTitle,postBody) {
+  var postData ={
+        title:postTitle,
+        body:postBody
+       }
+       var database = firebase.database().ref("post");
+
+       var newPostRef = database.push();
+       newPostRef.set(postData);
+      
+  }
+  
+
+function handleMessageFormSubmit(){
+  var postTitle= $("#post-title").val();
+  var postBody= $("#post-body").val();
+  addMessage(postTitle,postBody);
+  }
+  
+  
+
+function getweather(searchquery){
+var url = "https://api.openweathermap.org/data/2.5/weather?q="+searchquery+"&units=imperial&APPID="+apikey;
+
+  $(".city").text("");
+  $(".temp").text("");
+
+$.ajax(url,{success : function(data){
+  $(".city").text(data.name);
+  $(".temp").text(data.main.temp);
+}, error: function(error){
+  $(".error-message").text("An Error Occured");
+}
+})
+}
+
+function searchweather(){
+var searchquery= $(".search").val();
+getweather(searchquery);
+}
+
 function showPicture(){
   // use jQuery ($ is shorthand) to find the div on the page and then change the html
   // 'rounded-circle' is a bootstrap thing! Check out more here: http://getbootstrap.com/css/
